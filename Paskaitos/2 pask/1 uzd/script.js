87d5e5cd5bbfddde034e76a21cf9e94d57f7f7c6
VytisJ <vytis1133@gmail.com>
2023-10-02 19:37:51 +0300

    JAVA

---
+++ Paskaitos/2 pask/1 uzd/script.js
@@ -0,0 +1,33 @@
+// let x = 50;
+// let y = 10;
+// let division = x + y;
+// console.log(x+y);
+// console.log(x-y);
+// console.log(division)
+
+
+// console.log(2 === 2)
+// console.log(2 == "2")
+// console.log(2 !== 2)
+// console.log(5 < 5)
+// console.log(5 >= 4)
+// console.log(true !== false)
+// console.log(9 - 5 === 4)
+// console.log(5 * 5 !== "25")
+
+// const price = 120;
+// const money = 120;
+// if (money >= price){
+//   console.log("sveikinu nusipirkus preke");
+// } 
+
+let money = 100.50;
+let price = 105.50;
+
+if (money > price) {
+  console.log ("stai jusu graza."); // A
+} else if (money === price) {
+  console.log("you paid exact"); //B
+} else {
+  console.log("nepakanka pinigu"); //C
+}
